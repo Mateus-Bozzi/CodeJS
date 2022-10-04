d42ad554358117c22ace813b73a6fadaf2a60ea9
Teteuzins2 <mateuspvz1@gmail.com>
2022-10-03 22:34:32 -0300

    first commit

---
+++ RSS-News-Feed/app.js
@@ -0,0 +1,15 @@
+let url = 'http://feeds.bbci.co.uk/news/technology/rss.xml';
+const textarea = document.querySelector('#feed-textarea > ul');
+
+const date = new Date();
+document.querySelector('#date').innerHTML = date.toDateString();
+
+feednami.load(url).then(feed => {
+    textarea.value = '';
+    console.log(feed);
+    for(let entry of feed.entries){
+        let li = document.createElement('li');
+        li.innerHMTL = `<h4><a href ="${entry.link}">${entry.title}</a></h4>`;
+        textarea.appendChild(li);
+    }
+});
